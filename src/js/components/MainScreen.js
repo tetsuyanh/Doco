149b53cc4a0880a86d33960222809d60c67c8887
tetsuyanh <tetsuyanh@gmail.com>
2020-04-08 17:46:39 +0900

    use useState

--- src/js/components/MainScreen.js
+++ src/js/components/MainScreen.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 
 const MainScreen = () => {
+  const [count, setCount] = useState(0);
+
   return (
     <div>
       <h3>This is MainScreen ToDo</h3>
@@ -8,6 +10,8 @@ const MainScreen = () => {
         <li>use CSSModule</li>
         <li>CI Deploy</li>
       </ul>
+      <p>count: {count}</p>
+      <button onClick={() => setCount(count + 1)}>add</button>
     </div>
   );
 };
