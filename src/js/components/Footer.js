7c18bc3c38d66a30dcb3ce18350a896216904fb9
tetsuyanh <tetsuyanh@gmail.com>
2020-04-15 15:46:04 +0900

    use google fonts passero

--- src/js/components/Footer.js
+++ src/js/components/Footer.js
@@ -3,9 +3,7 @@ import React from 'react';
 import styles from './Footer.css';
 
 const Footer = () => {
-  return (
-    <footer className={styles.footer}>DocoDoco powered by Thwoo, Inc.</footer>
-  );
+  return <footer className={styles.footer}>Doco powered by Thwoo, Inc.</footer>;
 };
 
 export default Footer;
--- src/js/components/Header.js
+++ src/js/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 import styles from './Header.css';
 
 const Header = () => {
-  return <header className={styles.header}>DocoDoco</header>;
+  return <header className={styles.header}>Doco</header>;
 };
 
 export default Header;
